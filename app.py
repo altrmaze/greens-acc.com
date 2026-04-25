1d4da8e04f15a31173bbff61654ba2e5325e5b56
altrmaze <100169970+altrmaze@users.noreply.github.com>
2026-04-25 04:29:15 +0300

    Add Flask-Login integration for user management

--- app.py
+++ app.py
@@ -20,3 +20,11 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150), nullable=False)
 
 db.create_all()
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
